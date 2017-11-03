9f69d5980171b1f371844f90fdf030d8d94e5ce4
freezeburger <renaud.dubuis@decryptage.net>
2017-11-03 16:24:56 +0100

    Clean

---
+++ README.md
@@ -0,0 +1,16 @@
+# OGGRDesktop
+Desktop Version
+
+## Main Composant 
+* http://pineconellc.github.io/angular-foundation/
+* AngularJS
+* Firebase
+* MongoDB
+
+## Avoir
+* parse.com
+
+## MongoDB
+* modulus.io
+* mongolab
+* http://www.tutorialspoint.com/mongodb/mongodb_create_database.htm
---
+++ documentation/Everest/EverestSlide_files/google_analytics-8847c39974a91aa569631faf933d9f0f.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 17:18:50 janv. 25, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:46:49 juil. 3, 2015.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-24770715-7"]),_gaq.push(["_trackPageview"]),function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=("https:"==document.location.protocol?"/web/20130125171850/https://ssl":"/web/20130125171850/http://www")+".google-analytics.com/ga.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
